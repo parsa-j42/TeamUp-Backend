1ab670e9b48a82e293bf577852ab47fae16808b2
parsa_j42 <parsajfri42@gmail.com>
2025-04-19 19:09:50 -0600

    fix

--- src/applications/application.module.ts
+++ src/applications/application.module.ts
@@ -1,20 +1,22 @@
-import { Module, forwardRef } from '@nestjs/common'; // Add forwardRef import
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicationService } from './application.service';
 import { ApplicationController } from './application.controller';
 import { Application } from './application.entity';
 import { AuthModule } from '@auth/auth.module';
-import { ProjectModule } from '@projects/project.module'; // Import ProjectModule
-import { ProjectMembership } from '@projects/project-membership.entity'; // Import Membership entity
+import { ProjectModule } from '@projects/project.module';
+import { ProjectMembership } from '@projects/project-membership.entity';
+import { UserModule } from '@users/user.module'; // Add this import
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Application, ProjectMembership]), // Include Membership repo
-    AuthModule, // Provides JwtAuthGuard globally
+    TypeOrmModule.forFeature([Application, ProjectMembership]),
+    AuthModule,
     forwardRef(() => ProjectModule),
+    UserModule,
   ],
   controllers: [ApplicationController],
   providers: [ApplicationService],
-  exports: [ApplicationService], // Export if needed by other modules
+  exports: [ApplicationService],
 })
 export class ApplicationModule {}
